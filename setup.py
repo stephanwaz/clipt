fd2136646f9dbfdc523f5d772ac4daf314186bdb
stephen wasilewski <stephen@coolshadow.com>
2018-12-14 09:41:18 -0800

    rebump

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     tests_require=test_requirements,
     url='https://bitbucket.org/stephenwasilewski/clipt',
     project_urls= {'documentation': 'https://clipt.readthedocs.io/'},
-    version='0.1.4',
+    version='0.1.3',
     zip_safe=True,
 )
