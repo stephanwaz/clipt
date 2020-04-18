c293a632841cee0c66f971dc819b3be5bc8baacc
Stephen Wasilewski <stephanwaz@gmail.com>
2020-04-18 14:43:14 +0200

    Bump version: 1.0.7 → 1.0.8

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     tests_require=test_requirements,
     url='https://bitbucket.org/stephenwasilewski/clipt',
     project_urls= {'documentation': 'https://clipt.readthedocs.io/'},
-    version='1.0.7',
+    version='1.0.8',
     zip_safe=True,
 )
