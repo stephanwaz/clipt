c85112519c5ffca0e92e427187b1124299b0f7ed
Stephen Wasilewski <stephanwaz@gmail.com>
2022-07-28 20:00:36 +0200

    deprecation in click

--- clipt/cl_plot.py
+++ clipt/cl_plot.py
@@ -904,7 +904,7 @@ def colors(**kwargs):
     return 'colors', kwargs
 
 
-@plot.resultcallback()
+@plot.result_callback
 @click.pass_context
 def printconfig(ctx, opts, **kwargs):
     """callback to save config file"""
